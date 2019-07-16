b5f04ff4c0633e3c12aee8e86f7ee199bf42a639
Igor <igor9105@gmail.com>
2019-07-16 18:31:40 +0300

    Gamespace

---
+++ lesson_one_game/BaseObject.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace lesson_one_game
+{
+    /// <summary>
+    /// Базовый обект, от которого наследуются остальные
+    /// </summary>
+    abstract class BaseObject :ICollision
+    {
+        protected Point Pos;
+        protected Point Dir;
+        protected Size Size;
+
+
+        /// <summary>
+        /// Конструктор  базового объекта
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <param name="dir"></param>
+        /// <param name="size"></param>
+        public BaseObject(Point pos,Point dir,Size size)
+        {
+            Pos = pos;
+            Dir = dir;
+            Size = size;
+
+        }
+
+        /// <summary>
+        /// Отрисовка объекта будет реализована в классах-наследниках, здесь реализации нет, так как метод абстрактный
+        /// </summary>
+        public abstract void Draw();
+
+       
+
+        /// <summary>
+        /// Движение объекта
+        /// </summary>
+        public abstract void Update();
+        
+
+        public bool Collision(ICollision o) => o.Rect.IntersectsWith(this.Rect);
+        public Rectangle Rect => new Rectangle(Pos, Size);
+
+       
+    }
+}
---
+++ lesson_one_game/Game.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace lesson_one_game
+{
+     static class Game
+    {
+        private static BufferedGraphicsContext _context;
+        public static BufferedGraphics Buffer;
+
+        public static int Width { get; set; }
+        public static int Height { get; set; }
+
+        public static BaseObject[] _objs;
+
+        private static Bullet _bullet;
+        private static Asteroid[] _asteroids;
+
+        static Game()
+        {
+
+        }
+
+        
+        public static void Init(Form form)
+        {
+            Graphics g;
+            _context = BufferedGraphicsManager.Current;
+            g = form.CreateGraphics();
+            Width = form.ClientSize.Width;
+            Height = form.ClientSize.Height;
+            Buffer = _context.Allocate(g, new Rectangle(0, 0, Width, Height));
+            Load();
+            Timer timer = new Timer { Interval = 100 };
+            timer.Start();
+            timer.Tick += Timer_Tick;
+            
+        }
+
+        public static void Load()
+        {
+            _objs = new BaseObject[30];
+            _bullet = new Bullet(new Point(0, 200), new Point(5, 0), new Size(4, 1));
+            _asteroids = new Asteroid[3];
+            var rnd = new Random();
+            for (var i = 0; i < _objs.Length; i++)
+            {
+                int r = rnd.Next(5, 50);
+                _objs[i] = new Star(new Point(1000, rnd.Next(0, Game.Height)), new Point(-r, r), new Size(3, 3));
+            }
+            for (var i = 0; i < _asteroids.Length; i++)
+            {
+                int r = rnd.Next(5, 50);
+                _asteroids[i] = new Asteroid(new Point(1000, rnd.Next(0, Game.Height)), new Point(-r / 5, r), new Size(r, r));
+            }
+
+        }
+
+        private static void Timer_Tick(object sender,EventArgs e)
+        {
+            Draw();
+            Update();
+        }
+
+        public static void Draw()
+        {
+
+            Buffer.Graphics.Clear(Color.Black);
+            foreach (BaseObject obj in _objs)
+                obj.Draw();
+            foreach (Asteroid obj in _asteroids)
+                obj.Draw();
+            _bullet.Draw();
+            Buffer.Render();
+            
+        }
+
+        public static void Update()
+        {
+            foreach(BaseObject obj in _objs)
+        obj.Update();
+            foreach (Asteroid a in _asteroids)
+            {
+                a.Update();
+                if (a.Collision(_bullet)) { System.Media.SystemSounds.Hand.Play(); }
+            }
+            _bullet.Update();
+
+        }
+
+
+    }
+}
